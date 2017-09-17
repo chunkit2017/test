979e234630cca221371ba84aa00a133b30293624
chun kit <chunkit.com@gmail.com>
2017-09-17 10:48:57 +0800

    test

--- test.php
+++ test.php
@@ -2,7 +2,7 @@
 
     echo "LOGIN TEST Social";
     
-    echo "FInal REBASE TEst Git Branch first commit";
+    echo "FInal REBASE TEst Git Master 2nd Commit Git Branch first commit";
     
     echo "Git Branch first commit";
     
